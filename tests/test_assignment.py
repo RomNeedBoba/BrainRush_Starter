7ad6231b5a32a8bd5b83eeccb0a03491490d88b7
RomNeedBoba <rpphyrom.dev@gmail.com>
2026-05-28 13:46:19 +0700

    Initial classroom starter files

--- tests/test_assignment.py
+++ tests/test_assignment.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import subprocess, os
 
 def test_notebook_runs():
@@ -11,18 +10,5 @@ def test_notebook_runs():
     assert result.returncode == 0, result.stderr
 
 def test_csv_exists():
-=======
-import subprocess, os
 
-def test_notebook_runs():
-    # Execute the notebook end-to-end; fails if any cell errors
-    result = subprocess.run(
-        ["jupyter", "nbconvert", "--to", "notebook", "--execute",
-         "--output", "executed.ipynb", "assignment.ipynb"],
-        capture_output=True, text=True
-    )
-    assert result.returncode == 0, result.stderr
-
-def test_csv_exists():
->>>>>>> b546c9aa6d8409e301c1cff07bf822c28966b6d1
     assert os.path.exists("data.csv")
